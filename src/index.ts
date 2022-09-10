1f0f9816ca8fa800bb05ab1c72a8cc52012ebab2
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2022-09-10 10:39:46 -0300

    ✨ feat: Default route
    Default express route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 import express from "express";
+import { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) =>
+  res.send("Default route is working.")
+);
+
+app.listen(3000, () => {
+  console.log("Server running in http://localhost:3000");
+});
